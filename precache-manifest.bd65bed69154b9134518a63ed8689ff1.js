3828fc6dac7c2ab42210325d4595d24ec81aa959
Ayman Ali <aliayman@Aymans-MacBook-Pro13.local>
2020-05-19 09:27:16 -0700

    Updates

---
+++ precache-manifest.bd65bed69154b9134518a63ed8689ff1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db62c0d852dc34068fcf1e19515d223c",
+    "url": "/react-material-themes/index.html"
+  },
+  {
+    "revision": "943e214980873e6e4f13",
+    "url": "/react-material-themes/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "273561c37fdd0c5551cf",
+    "url": "/react-material-themes/static/js/2.cc7620fe.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-material-themes/static/js/2.cc7620fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "943e214980873e6e4f13",
+    "url": "/react-material-themes/static/js/main.c19d9d04.chunk.js"
+  },
+  {
+    "revision": "b7389c37169d3bac320c",
+    "url": "/react-material-themes/static/js/runtime-main.487ab0bc.js"
+  }
+]);
